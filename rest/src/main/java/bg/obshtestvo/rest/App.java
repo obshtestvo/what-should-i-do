af6ad40a6069595390ddaaa78d1c6ee3ffea8f68
unknown <milkomitropolitsky@gmail.com>
2014-06-01 16:14:22 +0300

    fixed project structure.

---
+++ rest/src/main/java/bg/obshtestvo/rest/App.java
@@ -0,0 +1,13 @@
+package bg.obshtestvo.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
