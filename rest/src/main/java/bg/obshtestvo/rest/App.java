f3722e6128813f25bb2b378274bf90990ebb5018
viktoriya-kutsarova <viktoriya.kutsarova@gmail.com>
2014-06-01 17:25:21 +0300

    Jersey and spring configuartions.

--- rest/src/main/java/bg/obshtestvo/rest/App.java
+++ rest/src/main/java/bg/obshtestvo/rest/App.java
@@ -2,12 +2,10 @@ package bg.obshtestvo.rest;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
