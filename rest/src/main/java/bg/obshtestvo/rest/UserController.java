7f9ec35bae122dd3c03cb0ff43f1b9012ea9d5d9
unknown <milkomitropolitsky@gmail.com>
2014-06-07 16:23:11 +0300

    Fixed: register endpoint response.

--- rest/src/main/java/bg/obshtestvo/rest/UserController.java
+++ rest/src/main/java/bg/obshtestvo/rest/UserController.java
@@ -4,12 +4,12 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import bg.obshtestvo.model.UserSecurityDetails;
@@ -34,14 +34,15 @@ public class UserController {
 				.build();
 	}
 	
+	@Path("/register")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/register")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response registerUser(String user) {
 		
 		Gson gson = new Gson();
 		UserSecurityDetails looser = gson.fromJson(user, UserSecurityDetails.class);
-		return Response.ok(userService.registerUser(looser), MediaType.APPLICATION_JSON)
+		return Response.ok(gson.toJson(userService.registerUser(looser)))
 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON + "; charset=utf-8")
 				.build();
 //		return Response.serverError().build();
